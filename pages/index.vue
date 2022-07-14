30f78b5f6b8b50b1a19620414b2cc4dbff93db45
Brian Daley <brian@uconn.edu>
2022-07-14 15:44:14 -0400

    switched to table view for occupations

--- pages/index.vue
+++ pages/index.vue
@@ -9,48 +9,63 @@
           Sed?</p>
         <h2>Begin by Choosing an Occupation</h2>
 
+        <v-data-table v-model="occupation" :items="occupations" :headers="table.headers" single-select
+          @item-selected="changed" show-select class="elevation-1" :search="search">
+          <template v-slot:top>
+
+            <v-text-field v-model="search" append-icon="mdi-magnify" label="Filter by Occupation" single-line
+              hide-details class="pa-5">
+            </v-text-field>
+
+
+          </template>
+
+        </v-data-table>
+
+
         <!-- UX Note: This might work better as a search box and results instead of autocomplete -->
-        <v-autocomplete v-model="chosenOccupation" :items="entries" :loading="isLoading" :search-input.sync="search"
-          hide-no-data hide-selected item-text="name" label="Occupations" placeholder="Start typing to Search"
-          prepend-icon="mdi-magnify" chips clearable return-object @change="changed"></v-autocomplete>
+        <!-- <v-autocomplete v-model=" chosenOccupation" :items="entries" :loading="isLoading"
+              :search-input.sync="search" hide-no-data hide-selected item-text="name" label="Occupations"
+              placeholder="Start typing to Search" prepend-icon="mdi-magnify" chips clearable return-object
+              @change="changed">
+              </v-autocomplete> -->
 
       </v-col>
     </v-row>
-    <v-row v-if="chosenOccupation !== null">
+    <v-row>
       <v-col>
         <!-- This table is ugly. Need something better -->
-        <v-simple-table>
+        <!-- <v-simple-table v-if="occupation">
           <template v-slot:default>
             <thead>
               <th>
-              <td>{{chosenOccupation.name}}</td>
+              <td>{{occupation.name}}</td>
               </th>
             </thead>
             <tbody>
               <tr>
                 <td>Starting Rate</td>
                 <td>
-                    {{ $money(chosenOccupation.hourly_rate_entry) }}/hr
+                  {{ $money(occupation.hourly_rate_entry) }}/hr
                 </td>
               </tr>
               <tr>
                 <td>Monthly Gross Income</td>
-                <td>{{ $money(chosenOccupation.monthly_gross) }}</td>
+                <td>{{ $money(occupation.monthly_gross) }}</td>
               </tr>
               <tr>
                 <td>Monthly Taxes</td>
-                <td>{{ $money(chosenOccupation.monthly_taxes)}}</td>
+                <td>{{ $money(occupation.monthly_taxes)}}</td>
               </tr>
             </tbody>
           </template>
-        </v-simple-table>
+        </v-simple-table> -->
 
       </v-col>
     </v-row>
     <v-row>
       <v-col>
-        <v-btn :disabled="chosenOccupation === null" to="/housing" color="secondary">Continue...</v-btn>
-
+        <v-btn :disabled="occupation.length < 1" to="/housing" color="secondary">Continue...</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -61,46 +76,43 @@ import { mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'IndexPage',
+  async asyncData({$supabase}) {
+    let result
+    try {
+      result = await $supabase.from('occupations').select('*')
+    } catch (error) {
+      console.log(error)
+    }
+    console.log(result)
+    return {
+      occupations: result.data,
+    }
+  },
   data(){
     return {
-      chosenOccupation: null,
       count: null,
-      entries: [],
-      items: [],
-      isLoading: null,
-      search: null
+      search: null,
+      occupation: [],
+      table: {
+        headers: [
+          {
+            text: 'Areas of Employment',
+            sortable: true,
+            value: 'name'
+
+          }
+
+        ]
+      }
     }
   },
   watch: {
     search(q) {
-      if (this.items.length > 0 || q.length <= 3) return
-
-      if (this.isLoading) return
-
-      this.isLoading = true
-
-
-      this.$supabase
-        .from('occupations')
-        .select('*')
-        .ilike('name', `*${q}*`)
-        .then(result => {
-          const { data } = result
-          this.count = data.length
-          this.entries = data
-        })
-        .catch(error => {
-          console.log(error)
-        })
-        .finally(() => {
-          this.isLoading = false
-          console.log(this.chosenOccupation)
-        })
 
     },
   },
   methods: {
-    changed(occupation){
+    changed({ item: occupation}){
       console.log('changed',occupation)
       this.set(occupation)
 
