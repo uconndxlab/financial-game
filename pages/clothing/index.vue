2a37a5c7e2c89afafb7b938542f1200fa39dad9a
Natalie <natalie.lacroix@uconn.edu>
2022-08-05 10:49:02 -0400

    level 5: food

--- pages/clothing/index.vue
+++ pages/clothing/index.vue
@@ -1,9 +1,9 @@
 <template>
 <v-container>
   <div>
-    <h1>Level 5: Food</h1>
+    <h1>Level 6: Clothing</h1>
     <p>*Make of CT goes here*</p>
-    <v-btn color="primary" to="/food/dietchoice">Play</v-btn>
+    <v-btn color="primary" to="/clothing/purchase">Play</v-btn>
   </div>
 </v-container>
 </template>
@@ -12,7 +12,7 @@
 import { mapActions } from 'vuex'
 
 export default {
-  name: 'FoodIndex',
+  name: 'ClothingIndex',
   mounted() {
     // Reset the store
     // this.reset()
---
+++ pages/food/dietchoice.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <h1>Food > Diet</h1>
+    <div class="choice-boxes text-center">
+    <p>Choose a diet:</p>
+    <v-btn-toggle v-model="selection" mandatory class="d-block" color="primary" @change="selectionChanged()">
+      <div v-for="option in dietOptions" :key="option.name" class="ma-4">
+        <v-btn width="100%" left>{{option.name}}</v-btn>
+      </div>
+
+    </v-btn-toggle>
+    </div>
+    <nav>
+      <ul class="nav-buttons">
+        <li>
+        </li>
+        <li>
+        <v-btn to="/food/budget" color="secondary">Continue...</v-btn>
+        </li>
+      </ul>
+    </nav>
+
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'DietChoice',
+  data(){
+    return {
+      selection: null,
+      dialog: false,
+      dietplan: null,
+      dietOptions: [
+        { name: 'No Diet', value: false},
+        { name: 'Organic', value: false},
+        { name: 'Gluten Free', value:false}
+
+      ],
+    }
+  },
+  methods: {
+    selectionChanged(){
+      this.dietplan = this.selection
+    }
+  }
+}
+</script>
