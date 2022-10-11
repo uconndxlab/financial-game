e74b9d7787d101cf603e4a8701e11d0e344dac59
Natalie <natalie.lacroix@uconn.edu>
2022-10-11 17:16:31 -0400

    new page

---
+++ pages/end.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container >
+    <v-row>
+      <v-col>
+        <h1>Congrats! Let's see how you did!</h1>
+        <p>About the game...</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi saepe nesciunt sapiente, culpa ex
+          ratione, quo eius in blanditiis dolorum voluptates quam illo eum praesentium reprehenderit sit, ipsum atque?
+          Sed?</p>
+        <h2>Budget Breakdown</h2>
+
+        
+
+
+      </v-col>
+    </v-row>
+
+    
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'EndPage'
+
+}
+</script>
